ef6b968e78fc4727114fbf5aec675bd8ad1b08d2
Illya <mario.polart@hotmail.fr>
2017-10-13 01:17:19 +0800

    ok

---
+++ src/AppBundle/Controller/VariableUrlController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class VariableUrlController extends Controller
+{
+  public function variableUrlAction(Request $request)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $path = $request->attributes->get('path');
+
+    if ($path == "recruteurs") {
+      return $this->render('AppBundle::recruteurs.html.twig', array('entity' => $em->getRepository('EpiDevAdminBundle:CMS')->find(1)));
+    }
+    else if ($path == "mentions_legales") {
+      return $this->render('AppBundle::mentions_legales.html.twig');
+    }
+    return $this->redirectToRoute('home');
+  }
+}
+
+?>
