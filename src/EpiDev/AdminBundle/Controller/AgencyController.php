f9dc963587b7c5f2b27446015e7167204135be01
Illya <mario.polart@hotmail.fr>
2017-10-15 04:30:37 +0800

    ok

--- src/EpiDev/AdminBundle/Controller/AgencyController.php
+++ src/EpiDev/AdminBundle/Controller/AgencyController.php
@@ -39,24 +39,28 @@ class AgencyController extends Controller
     ->add('save',      SubmitType::class);
 
     $form = $formBuilder->getForm();
-    return $this->render('EpiDevAdminBundle:Default:add_agency.html.twig', array(
-      'form' => $form->createView()));
-    }
+    return $this->render('EpiDevAdminBundle:Default:add_agency.html.twig', array('form' => $form->createView()));
+  }
 
-    public function editAction(Request $request)
-    {
-      $agency = new Agency();
+  public function editAction(Request $request)
+  {
+    $agencies = $this->getDoctrine()->getManager()->getRepository('EpiDevAdminBundle:Agency')->findAll();
+    $id = $request->query->get("id");
 
-      $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $agency);
-      $formBuilder
-      ->add('name',      TextType::class)
-      ->add('address',     TextType::class)
-      ->add('city',   TextType::class)
-      ->add('phone',    TextType::class)
-      ->add('save',      SubmitType::class);
+    return $this->render('EpiDevAdminBundle:Default:edit_agency.html.twig', array('agency' => $agencies[$id]));
+  }
 
-      $form = $formBuilder->getForm();
-      return $this->render('EpiDevAdminBundle:Default:add_agency.html.twig', array(
-        'form' => $form->createView()));
-      }
-    }
+  public function saveAction(Request $request)
+  {
+    $em = $this->getDoctrine()->getManager();
+    $agency = $em->getRepository('EpiDevAdminBundle:Agency')->find($request->request->get('id'));
+
+    $agency->setName($request->request->get('name'));
+    $agency->setAddress($request->request->get('address'));
+    $agency->setCity($request->request->get('city'));
+    $agency->setPhone($request->request->get('phone'));
+    $em->flush();
+    return $this->redirectToRoute('agency');
+  }
+
+}
