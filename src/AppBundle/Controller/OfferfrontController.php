bc72715a5f3e5f517781cfc38928370f10d1f937
Illya <mario.polart@hotmail.fr>
2017-10-23 14:35:09 +0800

    ok

--- src/AppBundle/Controller/OfferfrontController.php
+++ src/AppBundle/Controller/OfferfrontController.php
@@ -5,15 +5,24 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class OfferfrontController extends Controller
 {
+  public $i = 0;
+
   public function listAction(Request $request)
   {
     $em = $this->getDoctrine()->getManager();
     $offers = $em->getRepository('EpiDevAdminBundle:Offer')->findAll();
 
     return $this->render('AppBundle::offer.html.twig', array('offers' => $offers));
+  }
+
+  public function inc_i_offerAction()
+  {
+    $this->i = $this->i+1;
+    return $this->render('AppBundle::offer.html.twig');
   }
 
   public function detailsAction(Request $request)
