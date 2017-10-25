482bff21ae3bf39a88e7058401b9e95e557e4eab
Illya <mario.polart@hotmail.fr>
2017-10-25 16:58:43 +0800

    ok

--- src/AppBundle/Controller/OfferfrontController.php
+++ src/AppBundle/Controller/OfferfrontController.php
@@ -14,35 +14,66 @@ class OfferfrontController extends Controller
   {
     $em = $this->getDoctrine()->getManager();
     $offers = $em->getRepository('EpiDevAdminBundle:Offer')->findAll();
-
-    return $this->render('AppBundle::offers.html.twig', array('offers' => $offers));
-  }
-
-  public function inc_i_offerAction(Request $request)
-  {
-    $i = $request->request->get('i');
-    $em = $this->getDoctrine()->getManager();
-    $offers = $em->getRepository('EpiDevAdminBundle:Offer')->findAll();
-    $title = $offers[$i]->getTitle();
-    $id = $offers[$i]->getId();
-    $description = $offers[$i]->getDescription_txt();
-    $city = $offers[$i]->getCity();
-    $type = $offers[$i]->getJob_type();
-    $domain = $offers[$i]->getDomain();
-
-    return new Response(json_encode(array('title' => $title, 'description' => $description, 'id' => $id, 'city' => $city, 'type' => $type, 'domain' => $domain)));
-  }
-
-  public function detailsAction(Request $request)
-  {
-    $em = $this->getDoctrine()->getManager();
-    $offer = $em->getRepository('EpiDevAdminBundle:Offer')->find($request->query->get('id'));
-    $agencies = $em->getRepository('EpiDevAdminBundle:Agency')->findAll();
-    $cms = $em->getRepository('EpiDevAdminBundle:CMS')->findAll()[0]->getUrlName();
+    $keyword = $request->query->get('keyword');
+    $city = $request->query->get('city');
+    $type = $request->query->get('type');
+    $domain = $request->query->get('domain');
+    /*if ($keyword) {
     $query = $em->createQuery(
-        'SELECT p
-        FROM EpiDevAdminBundle:Offer p
-                    ORDER BY p.id DESC'
+    'SELECT p FROM EpiDevAdminBundle:Offer p WHERE p.city LIKE :keyword'
+    )->setParameters(array('keyword' => "%".$keyword."%")
+  );
+  $offers = $query->getResult();
+}*/
+if ($city) {
+  //var_dump($city);
+  $query = $em->createQuery(
+    'SELECT p FROM EpiDevAdminBundle:Offer p WHERE p.city = :city'
+    )->setParameters(array('city' => $city)
+  );
+  $offers = $query->getResult();
+}
+/*if ($type) {
+$query = $em->createQuery(
+'SELECT p FROM EpiDevAdminBundle:Offer p WHERE p.job_type LIKE :job'
+)->setParameters(array('job' => '%'.$type.'%')
+);
+$offers = $query->getResult();
+}
+if ($domain) {
+$query = $em->createQuery(
+'SELECT p FROM EpiDevAdminBundle:Offer p WHERE p.domain LIKE :domain'
+)->setParameters(array('domain' => '%'.$domain.'%')
+);
+$offers = $query->getResult();
+}*/
+
+//var_dump($offers);
+return $this->render('AppBundle::offers.html.twig', array('offers' => $offers));
+}
+
+public function inc_i_offerAction(Request $request)
+{
+  $i = $request->request->get('i');
+  $em = $this->getDoctrine()->getManager();
+  $offers = $em->getRepository('EpiDevAdminBundle:Offer')->findAll();
+  $title = $offers[$i]->getTitle();
+  $id = $offers[$i]->getId();
+  $description = $offers[$i]->getDescription_txt();
+
+  return new Response(json_encode(array('title' => $title, 'description' => $description, 'id' => $id)));
+}
+
+public function detailsAction(Request $request)
+{
+  $em = $this->getDoctrine()->getManager();
+  $offer = $em->getRepository('EpiDevAdminBundle:Offer')->find($request->query->get('id'));
+  $agencies = $em->getRepository('EpiDevAdminBundle:Agency')->findAll();
+  $cms = $em->getRepository('EpiDevAdminBundle:CMS')->findAll()[0]->getUrlName();
+  $query = $em->createQuery(
+    'SELECT p
+    FROM EpiDevAdminBundle:Offer p
+    ORDER BY p.id DESC'
     )->setMaxResults(4);
     $last_offers = $query->getResult();
 
